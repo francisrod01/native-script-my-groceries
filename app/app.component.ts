c2583d69c0d016526dc4759bd6af3f61de1205ab
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-25 19:49:32 -0300

    ENH: Refactored login template

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,39 +4,7 @@ import { User } from './shared/user/user';
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image
-        src="res://logo_login"
-        stretch="none"
-        horizontalAlignment="center"
-      ></Image>
-      
-      <TextField
-        hint="Email Address"
-        keyboardType="email"
-        [(ngModel)]="user.email"
-        autocorrect="false"
-        autocaptalizationType="none"
-      ></TextField>
-
-      <TextField
-        hint="Password"
-        secure="true"
-        [(ngModel)]="user.password"
-      ></TextField>
-
-      <Button
-        [text]="isLoggingIn ? 'Sign in' : 'Sign up'"
-        class="submit-button"
-        (tap)="submit()"
-      ></Button>
-      <Button
-        [text]="isLoggingIn ? 'Sign up' : 'Back to login'"
-        (tap)="toggleDisplay()"
-      ></Button>
-    </StackLayout>
-  `,
+  templateUrl: "./pages/login/login.html",
   styleUrls: [
     "pages/login/login-common.css",
     "pages/login/login.css"
