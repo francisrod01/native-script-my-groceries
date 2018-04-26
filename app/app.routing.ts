eaf88dcb73e70feeb6f795e2af494ffebd81df11
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-26 11:20:15 -0300

    Setting up routing initializing with LoginComponent

---
+++ app/app.routing.ts
@@ -0,0 +1,10 @@
+import { LoginComponent } from "./pages/login/login.component";
+
+
+export const routes = [
+  { path: "", component: LoginComponent }
+];
+
+export const navigatableComponents = [
+  LoginComponent
+];
