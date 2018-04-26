7880ac08788bc7e2063a87e57e5df0aba85bfdc5
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-26 11:34:09 -0300

    Added ListComponent

---
+++ app/pages/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "list",
+  moduleId: module.id,
+  templateUrl: "./list.html",
+  styleUrls: [
+    "./list-commons.css",
+    "./list.css"
+  ]
+})
+export class ListComponent {}
