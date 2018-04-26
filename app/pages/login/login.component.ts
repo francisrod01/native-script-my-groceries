b57d61b3ce08a1aa6ff216e68d4ef8230f0af9e6
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-26 11:19:04 -0300

    ENH: Refactored LoginComponent

---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from "@angular/core";
+
+import { UserService } from '../../shared/user/user.service';
+import { User } from '../../shared/user/user';
+
+@Component({
+  selector: "login",
+  providers: [
+    UserService
+  ],
+  templateUrl: "./pages/login/login.html",
+  styleUrls: [
+    "pages/login/login-common.css",
+    "pages/login/login.css"
+  ]
+})
+export class LoginComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private userService: UserService) {
+    this.user = new User();
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // @TODO: Define.
+  }
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => {
+          alert("Unfortunately we were unable to create your account.");
+        }
+      );
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
