ec0e9383f1b1d56668876819cad5924cfd9df073
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-26 17:43:19 -0300

    Implemented a textfield input to add groceries

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { TextField } from "ui/text-field";
 
 import { Grocery } from "../../shared/grocery/grocery";
 import { GroceryListService } from "../../shared/grocery/grocery-list.service";
@@ -17,6 +18,8 @@ import { GroceryListService } from "../../shared/grocery/grocery-list.service";
 })
 export class ListComponent implements OnInit {
   groceryList: Array<Grocery> = [];
+  grocery = ""
+  @ViewChild("groceryTextField") groceryTextField: ElementRef;
 
   constructor(private groceryListService: GroceryListService) {}
 
@@ -27,5 +30,31 @@ export class ListComponent implements OnInit {
           this.groceryList.unshift(groceryObject);
         });
       });
+  }
+
+  add() {
+    if (this.grocery.trim() === "") {
+      alert("Enter a grocery item");
+      return;
+    }
+
+    // Dismiss the keyboard
+    const textField = <TextField>this.groceryTextField.nativeElement;
+    textField.dismissSoftInput();
+
+    this.groceryListService.add(this.grocery)
+      .subscribe(
+          groceryObject => {
+          this.groceryList.unshift(groceryObject);
+          this.grocery = "";
+        },
+        () => {
+          alert({
+            message: "An error occurred while adding an item to your list.",
+            okButtonText: "OK"
+          });
+          this.grocery = "";
+        }
+      )
   }
 }
